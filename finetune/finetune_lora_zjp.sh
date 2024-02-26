c7fe9f798f99a64853a946f8b8fd4ff18a5c0518
doodleliang <doodleliang@tencent.com>
2024-02-26 15:26:44 +0800

    feat(ci): train zjp

---
+++ finetune/finetune_lora_zjp.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+DIR=`pwd`
+
+# Guide:
+# This script supports distributed training on multi-gpu workers (as well as single-worker training).
+# Please set the options below according to the comments.
+# For multi-gpu workers training, these options should be manually set for each worker.
+# After setting the options, please run the script on each worker.
+
+# Number of GPUs per GPU worker
+GPUS_PER_NODE=8
+
+# Number of GPU workers, for single-worker training, please set to 1
+NNODES=1
+
+MODEL="/mnt/cephfs/LLM_MODEL_HUB/qwen/Qwen-72B-Chat" # Set the path if you do not want to load from huggingface directly
+# ATTENTION: specify the path to your training data, which should be a json file consisting of a list of conversations.
+# See the section for finetuning in README for more information.
+DATA="data/zjp_train.json"
+DS_CONFIG_PATH="finetune/ds_config_zero2.json"
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+torchrun $DISTRIBUTED_ARGS finetune.py \
+    --model_name_or_path $MODEL \
+    --data_path $DATA \
+    --bf16 True \
+    --output_dir /mnt/cephfs/doodleliang/model/zjp_qwen_72b \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 200 \
+    --save_total_limit 2 \
+    --learning_rate 3e-4 \
+    --weight_decay 0.1 \
+    --adam_beta2 0.95 \
+    --warmup_ratio 0.01 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --report_to "none" \
+    --model_max_length 3000 \
+    --lazy_preprocess True \
+    --use_lora \
+    --gradient_checkpointing \
+    --deepspeed ${DS_CONFIG_PATH}
---
+++ zjp_to_qwen.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os 
+import codecs
+import json
+import random
+
+main_dir = '/mnt/chongqinggeminiceph1fs/geminicephfs/security-others-common'
+local_dir = f'{main_dir}/doodleliang/data_hub/zjp'
+
+cur_id = 0
+final_result = []
+for file in os.listdir(local_dir):
+    cur_file = os.path.join(local_dir, file)
+    for line in codecs.open(cur_file, 'r', encoding='utf-8'):
+        cur_id += 1
+        cur_obj = json.loads(line.strip())
+        question = cur_obj['question']
+        answer = cur_obj['answer']
+        ret_obj = {
+            "id": str(cur_id),
+            "conversations": [
+                {
+                    "from": "user",
+                    "value": question
+                },
+                {
+                    "from": "assistant",
+                    "value": answer
+                }
+            ]
+        }
+        final_result.append(ret_obj)
+random.shuffle(final_result)
+with codecs.open('data/zjp_train.json', 'w', encoding='utf-8') as f:
+    json.dump(final_result, f, ensure_ascii=False, indent=2)
