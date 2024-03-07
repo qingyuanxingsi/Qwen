2038dc6fd6b6300752e12a37b1a3cffaf0065e48
doodleliang <doodleliang@tencent.com>
2024-03-07 11:37:16 +0800

    feat(ci): update qwen

--- zjp_to_qwen.py
+++ zjp_to_qwen.py
@@ -11,6 +11,8 @@ local_dir = f'{main_dir}/doodleliang/data_hub/zjp'
 cur_id = 0
 final_result = []
 for file in os.listdir(local_dir):
+    if 'aug' in file:
+        continue
     cur_file = os.path.join(local_dir, file)
     for line in codecs.open(cur_file, 'r', encoding='utf-8'):
         cur_id += 1
@@ -32,5 +34,6 @@ for file in os.listdir(local_dir):
         }
         final_result.append(ret_obj)
 random.shuffle(final_result)
+print(f"total {len(final_result)} gen")
 with codecs.open('data/zjp_train.json', 'w', encoding='utf-8') as f:
     json.dump(final_result, f, ensure_ascii=False, indent=2)
